0d09e79891d683902a21505318132efef5d642f5
Roberto Alfredo Meza Meza <mezamezar87@univie.ac.at>
2022-11-30 11:51:18 +0100

    New Project based on Kotlin/JVM

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+/*
+ * This Kotlin/JVM buildscript was given to me by my great colluege Raphael
+ * https://github.com/RaphaelTarita
+ */
+
+
 plugins {
-    kotlin("multiplatform") version "1.7.10"
+    kotlin("jvm") version "1.7.21"
 }
 
-group = "me.roberto"
+group = "com.rmezameza"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+dependencies {
+    // For libraries
+}
 
-    nativeTarget.apply {
-        binaries {
-            executable {
-                entryPoint = "main"
-            }
-        }
-    }
-    sourceSets {
-        val nativeMain by getting
-        val nativeTest by getting
-    }
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
 }
---
+++ src/main/kotlin/com/rmezameza/Main.kt
@@ -0,0 +1,5 @@
+package com.rmezameza
+
+fun main() {
+        println("Hello World")
+}
